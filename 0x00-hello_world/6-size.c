783cf5af6266b228bd69615acb09c3fab3a1aa04
Manjjoro <andersonmain@proton.me>
2022-10-30 12:48:19 +0300

    deleted comments

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,12 +11,3 @@ int main(void)
     return (0);
 }
 
-// Output
-/*
-Size of a char: 1 byte(s)
-Size of an int: 4 byte(s)
-Size of a long int: 8 byte(s)
-Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)
-
-*/
