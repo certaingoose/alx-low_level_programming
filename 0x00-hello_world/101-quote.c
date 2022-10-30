68753d2c0a8cb3cfc646064d1090ccaa8b47f291
Manjjoro <andersonmain@proton.me>
2022-10-30 15:05:49 +0300

    deleted comments

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    char artisusf[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(1, artisusf, 60);
-    return (1);
+char artisusf[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, artisusf, 60);
+return (1);
 }
